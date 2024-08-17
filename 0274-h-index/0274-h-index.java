9cc7194e497e661f923a2fdc2df44d2fc83e1dfd
Himanshu Singh <84956771+Himanshu3179@users.noreply.github.com>
2024-08-17 16:06:03 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (19.34%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int []arr = new int[n+1];
+        for(int i:citations){
+            if(i>n){
+                arr[n]++;
+            }
+            else{
+                arr[i]++;
+            }
+        }
+        int count=0;
+        for(int i=n;i>=0;i--){
+            count+= arr[i];
+            if(count>=i){
+                return i;
+            }
+        }
+        return 0;
+
+    }
+}
