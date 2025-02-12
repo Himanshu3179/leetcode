b5c6d6b4773fc8f6cb3e8cc34ee5ec0f086fdb91
Himanshu Singh <84956771+Himanshu3179@users.noreply.github.com>
2025-02-12 16:33:44 +0530

    Time: 13 ms (6.74%), Space: 44.4 MB (75.29%) - LeetHub

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -0,0 +1,24 @@
+    class Solution {
+    public int minimumOperations(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
+        int max = -1;
+        for(int i=0;i<nums.length;i++){
+            int n = nums[i];
+            if(map.containsKey(n)){
+                set.add(map.get(n)/3);
+                map.put(n,i);
+            }
+            else{
+                map.put(n,i);
+            }
+        }
+        System.out.println(map);
+        System.out.println(set);
+        for(int i:set){
+            max = Math.max(i,max);
+        }
+ 
+        return (max==-1)?0:max+1;
+    }
+}
